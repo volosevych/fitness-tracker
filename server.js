03c1a03ddf48a2127f08430101846a4860e7ebf0
volosevych <vestynkas@gmail.com>
2020-11-19 19:55:34 -0700

    add paths to server

--- server.js
+++ server.js
@@ -24,6 +24,20 @@ mongoose.connect(
     }
   );
 
+require("./routes/api-routes.js")(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 });
