ffcd4dd835f76a2259207c36412f665b8c38ba1e
volosevych <vestynkas@gmail.com>
2020-11-19 19:54:33 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(expres.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+});
+  
